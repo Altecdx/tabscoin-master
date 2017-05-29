037698d7cd19dfe7a29e6ad97a518c5605e15405
Admin <admin@coin.com>
2017-05-29 09:19:03 +0200

    TabsCoin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define TabsCoin-qt message queue name
+#define BITCOINURI_QUEUE_NAME "tabscoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
